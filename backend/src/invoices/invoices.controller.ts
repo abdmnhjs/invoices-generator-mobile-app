a4acb747860984f99b348ceb691f24017ae47a7a
Abdelaziz <chaaraouiabdelaziz@gmail.com>
2025-08-21 16:14:20 +0200

    ajout du pipe pr parser

--- backend/src/invoices/invoices.controller.ts
+++ backend/src/invoices/invoices.controller.ts
@@ -4,15 +4,20 @@ import {
   Body,
   HttpException,
   HttpStatus,
+  UsePipes,
 } from '@nestjs/common';
 import { InvoicesService } from './invoices.service';
 import type { CreateInvoiceDto } from './dto/create-invoice.dto';
+import { ZodValidationPipe } from '../common/pipes/zod-validation.pipe';
+
+import { invoiceSchema } from '../../../lib/schemas/invoice-schema';
 
 @Controller('invoices')
 export class InvoicesController {
   constructor(private invoicesService: InvoicesService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(invoiceSchema))
   createInvoice(@Body() createInvoiceDto: CreateInvoiceDto) {
     try {
       return this.invoicesService.createInvoice(createInvoiceDto);
